9323714c75e762beba33b2b4367cba6827f0206d
유국현 <yoogukhyeon@yugughyeon-ui-iMac.local>
2023-08-28 11:35:57 +0900

    feat: rss 추가 & 글 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,72 @@ module.exports = {
     image: 'https://news.everyday-365.com/profile-image.png',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map((node) => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  // 사이트 주소(post 주소포함)에 한글이 있는경우 encodeURI을 사용합니다.
+                  // slug는 사이트의 post주소을 이름으로 씁니다. gatsby-node.js와 연관 있습니다.
+                  url: encodeURI(site.siteMetadata.siteUrl + node.fields.slug),
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ 'content:encoded': node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  nodes {
+                    excerpt
+                    html
+                    fields {
+                      slug
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+            // 최종 rss feed파일 입니다. 디렉토리가 다르거나, 이름이 다른경우 설정 가능합니다.
+            output: '/rss.xml',
+            // 본인의 blog rss feed용 타이틀을 명시합니다.
+            title: "Your Site's RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            // 전체 site주소에서 특정 주소 패턴만 rss feed생성해주는 부분입니다. 옵션널한 설정으로, 전체 사이트 rss시에는 제거해도 무방합니다.
+            match: '^/blog/',
+            // optional configuration to specify external rss feed, such as feedburner
+            // feedburner같은 외부 rss feed서비스 사용시에 씁니다. 미사용시 제거합니다.
+            link: 'https://feeds.feedburner.com/gatsby/blog',
+          },
+        ],
+      },
+    },
     'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-robots-txt',
