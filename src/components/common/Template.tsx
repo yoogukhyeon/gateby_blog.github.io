a1261fbbcdf0af373291211f5a49a10015c3634f
yoogukhyeon <rnrgus5897@gmail.com>
2023-10-02 17:51:49 +0900

    feat: canonical 추가

--- src/components/common/Template.tsx
+++ src/components/common/Template.tsx
@@ -52,6 +52,7 @@ const Template: FunctionComponent<TemplateProps> = function ({ title, descriptio
         <html lang="ko" />
         <link rel="shortcut icon" href="favicon.png" />
         <link rel="apple-touch-icon-precomposed" href="apple-touch-icon.png" />
+        <link rel="canonical" href="https://news.everyday-365.com" />
       </Helmet>
 
       <GlobalStyle />
